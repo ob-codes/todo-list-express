2f0d3a31b87118749fb05f3c8f74e0e48e45bc91
owaisbukhari00 <owaisbukhari@outlook.com>
2023-10-28 03:37:52 +0500

    updated formatting

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const PORT = 2121;
+require('dotenv').config();
 
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+    dbName = 'todo';
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-    })
+        console.log(`Connected to ${dbName} Database`);
+        db = client.db(dbName);
+    });
     
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
 app.get('/',async (request, response)=>{
