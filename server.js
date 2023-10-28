4a7ef1496903d31891760c851a94c55a3bc48f42
owaisbukhari00 <owaisbukhari@outlook.com>
2023-10-29 00:50:29 +0500

    updated express module setting for vercel

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use(express.json());
 
 
 app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+    const todoItems = await db.collection('todos').find().toArray();
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false});
+    response.render('index.ejs', { items: todoItems, left: itemsLeft });
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -33,16 +33,16 @@ app.get('/',async (request, response)=>{
     //     })
     // })
     // .catch(error => console.error(error))
-})
+});
 
 app.post('/addTodo', (request, response) => {
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
     .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
+        console.log('Todo Added');
+        response.redirect('/');
     })
-    .catch(error => console.error(error))
-})
+    .catch(error => console.error(error));
+});
 
 app.put('/markComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
@@ -54,12 +54,11 @@ app.put('/markComplete', (request, response) => {
         upsert: false
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked Complete');
+        response.json('Marked Complete');
     })
-    .catch(error => console.error(error))
-
-})
+    .catch(error => console.error(error));
+});
 
 app.put('/markUnComplete', (request, response) => {
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
@@ -71,23 +70,24 @@ app.put('/markUnComplete', (request, response) => {
         upsert: false
     })
     .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+        console.log('Marked Complete');
+        response.json('Marked Complete');
     })
-    .catch(error => console.error(error))
-
-})
+    .catch(error => console.error(error));
+});
 
 app.delete('/deleteItem', (request, response) => {
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
     .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
+        console.log('Todo Deleted');
+        response.json('Todo Deleted');
     })
-    .catch(error => console.error(error))
-
-})
+    .catch(error => console.error(error));
+});
 
 app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
+
+// Export the Express API
+module.exports = app;
